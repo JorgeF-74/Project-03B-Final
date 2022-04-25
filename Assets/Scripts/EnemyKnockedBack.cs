06772a6f89d4a9a91f8e952d4d36c957484cef86
Jorge F <jaf170330@utdallas.edu>
2022-04-24 21:04:19 -0500

    Enemy Knockback improvements
    Now enemies who are knocked back, will knock back other enemies that they collide with, causing a possible domino effect.

--- Assets/Scripts/EnemyKnockedBack.cs
+++ Assets/Scripts/EnemyKnockedBack.cs
@@ -9,13 +9,16 @@ public class EnemyKnockedBack : MonoBehaviour
     EnemyThrown thrownState;
 
     float KnockForce = 300f;
+    float W_KnockForce = 250f;
+
+    public bool E_KnockedBack = false;
 
     [SerializeField] GameObject EnemySelf;
     
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy") && other.GetComponent<EnemyThrown>().E_Thrown == true) //
+        if (other.CompareTag("Enemy") && other.GetComponent<EnemyThrown>().E_Thrown == true) 
         {
 
             transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
@@ -23,11 +26,22 @@ public class EnemyKnockedBack : MonoBehaviour
             EnemySelf.GetComponent<Rigidbody>().AddForce(other.transform.forward * KnockForce);
             Enemy_KnockedOff();
         }
+
+        if (other.CompareTag("Enemy") && other.GetComponent<EnemyKnockedBack>().E_KnockedBack == true)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+
+            EnemySelf.GetComponent<Rigidbody>().AddForce(-other.transform.forward * W_KnockForce);
+            Enemy_KnockedOff();
+        }
+
+
     }
 
     void Enemy_KnockedOff()
     {
         Debug.Log("2nd enemy has been knocked off feet!");
+        E_KnockedBack = true;
         
     }
 
