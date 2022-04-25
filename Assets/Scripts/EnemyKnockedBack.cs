586ad92b6c7dd64aba59781391406956416f9961
Jorge F <jaf170330@utdallas.edu>
2022-04-24 20:27:33 -0500

    Added Enemy Knockback Mechnanic
    Now when an enemy is throw towards another enemy, the other enemy gets knocked back!

--- Assets/Scripts/EnemyKnockedBack.cs
+++ Assets/Scripts/EnemyKnockedBack.cs
@@ -8,15 +8,27 @@ public class EnemyKnockedBack : MonoBehaviour
 
     EnemyThrown thrownState;
 
+    float KnockForce = 300f;
+
+    [SerializeField] GameObject EnemySelf;
+    
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy")) //&& thrownState.E_Thrown == true
+        if (other.CompareTag("Enemy") && other.GetComponent<EnemyThrown>().E_Thrown == true) //
+        {
+
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z);
+
+            EnemySelf.GetComponent<Rigidbody>().AddForce(other.transform.forward * KnockForce);
             Enemy_KnockedOff();
+        }
     }
 
     void Enemy_KnockedOff()
     {
         Debug.Log("2nd enemy has been knocked off feet!");
+        
     }
 
 }
